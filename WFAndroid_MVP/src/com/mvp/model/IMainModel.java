89679e3ab726960d3f50c56aab11f8c54a8a8d6e
CaoPengfei <whiskeyfei@users.noreply.github.com>
2015-12-13 14:08:51 +0800

    update mvp

---
+++ WFAndroid_MVP/src/com/mvp/model/IMainModel.java
@@ -0,0 +1,9 @@
+package com.mvp.model;
+
+
+public interface IMainModel {
+	/**
+	 * 显示文字逻辑实现
+	 */
+	void show();
+}
---
+++ WFAndroid_MVP/src/com/mvp/presenter/IMainCallback.java
@@ -0,0 +1,9 @@
+package com.mvp.presenter;
+
+public interface IMainCallback {
+	/**
+	 * 显示结果文字 
+	 * @param string
+	 */
+	void show(String string);
+}
